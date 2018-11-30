243106abcc101c8aa237e4d5035a03d6d71d62e1
charlie-becker <charliebecker@u.boisestate.edu>
2018-11-30 00:17:05 -0700

    Initial Merge with Time Series (Dygraph)

--- app.R
+++ app.R
@@ -1,17 +1,39 @@
+library(xts)
 library(ncdf4)
 library(shiny)
 library(leaflet)
 library(raster)
 library(rjson)
 library(sp)
+library(shinyWidgets)
+library(dplyr)
+library(htmlwidgets)
+library(dygraphs)
 
 source("buttonIndicator.R")
+
+d <- read.csv("/Users/charlesbecker/Desktop/Data/Project Data/Shiny/30YR_Stats/AVA_30YR_NoLeap917.csv")
+dates <- seq(as.Date("1987-01-01"),as.Date("1987-12-31"), "day")
+
+#the axis label is passed as a date, this function outputs only the month of the date
+getMonth <- 'function(d){
+var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+return monthNames[d.getMonth()];
+}'
+
+#the x values are passed as milliseconds, turn them into a date and extract month and day
+getMonthDay <- 'function(d) {
+var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+date = new Date(d);
+return monthNames[date.getMonth()] + " " +date.getUTCDate(); }'
+
+scaleType <- c("Individual Years", "Monthly (across all years)", "All years")
 
 setwd("/Users/charlesbecker/Desktop/Data/30YR_Daily/Data/")
 
 monNames = c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
 
-# get netCDF data
+# get netCDF datamb 
 ncFileNames <- list.files(pattern = ".nc")
 
 # initial raster
@@ -61,7 +83,7 @@ map = leaflet() %>% addTiles() %>%
 ################################################################################
 
 ui <- navbarPage("SRAVA",
-        tabPanel("Explorer",             
+    tabPanel("Explorer",             
     useShinyjs(),
     tags$style(appCSS),
     titlePanel("Snake River Valley American Viticultural Area"),
@@ -82,7 +104,26 @@ ui <- navbarPage("SRAVA",
             leafletOutput("myMap", width = "900", height = "650"),
             br(),br()
         ))),
-        tabPanel("TimeSeries"),
+        tabPanel("TimeSeries",
+                 titlePanel("Time series statistics"),
+                 sidebarLayout(
+                     sidebarPanel(
+                         selectInput("domainInput1", "Domain",
+                                     choices = c("Snake River AVA", "Sunnyslope", "Domain 02", "Domain 01"),  
+                                     selected = "Snake River AVA"),
+                         selectInput("scaleInput","What would you like to plot?", 
+                                     choices = scaleType, selected = scaleType[1] ),
+                         pickerInput("yearInput1", "Years", choices = 1987:2017, selected = "1995", multiple = T,
+                                     options =  list("max-options" = 5)),
+                         selectInput("varInput1", "Variable", choices = varNamesLong, selected = varNamesLong[3]),
+                         radioButtons("unitInput", "Unit type", 
+                                      choices = c("Fahrenheit/Inches", "Celcius/mm"), selected = "Fahrenheit/Inches")),
+                     
+                     mainPanel(
+                         dygraphOutput("myGraph", width = "900", height = "650"),
+                         br(),br()
+                     )
+                 )),
         tabPanel("Stats"),
         tabPanel("Background")
     
@@ -91,6 +132,10 @@ ui <- navbarPage("SRAVA",
 ################################################################################
 
 server = function(input, output, session) {
+    
+###############################################################################
+#  The follwing section refers to the "Explorer" Tab Panel 
+    
     
     # render leaflet map
     output$myMap <- renderLeaflet(map)
@@ -154,9 +199,7 @@ server = function(input, output, session) {
     rev_color_pal <- eventReactive(input$button, { colorNumeric(rev(c("dark red", "light blue", "dark green")), values(rast2()),
                                               na.color = "transparent") })
     
-    
-    
-    # create proxy leaflet map (from original map) 
+        # create proxy leaflet map (from original map) 
     observeEvent(input$button, {
         withBusyIndicatorServer("button", { 
         leafletProxy("myMap", data = rast2()) %>%
@@ -169,9 +212,29 @@ server = function(input, output, session) {
                           labelFormat(transform = function(x) sort(x, decreasing = TRUE)))})
     })
     
-    # Kill the app when closed in the browser 
-    session$onSessionEnded(stopApp)
+
+
+###############################################################################
+## The Following section refers to the "Time series" tabPanel
+
+vv <- reactive ({ ncVarNames[match(input$varInput1, varNamesLong)] })
+
+dd <- reactive ({ if (vv() == "TMAX" | vv() == "TMIN" | vv() == "TMEAN") {
+    
+    as.data.frame(bind_cols(split(d[[vv()]], d$YEAR)) %>% select(input$yearInput1)
+    )}
+    else {cumsum(as.data.frame(bind_cols(split(d[[vv()]], d$YEAR)) %>% select(input$yearInput1))) }
+})
+
+df <- reactive ({ data.frame(dates, dd()) })
+
+df_dy <- reactive ({ xts(df(), order.by = dates) })
+
+output$myGraph <- renderDygraph({ dygraph(df_dy()) %>% 
+        dyAxis("x", axisLabelFormatter = JS(getMonth), valueFormatter = JS(getMonthDay)) })
+
+# Kill the app when closed in the browser 
+session$onSessionEnded(stopApp)
 }
 
 shinyApp(ui, server)
-
