7bf15042ef92ac6f4d49d3722739d737c6d1b58b
charlie-becker <charliebecker@u.boisestate.edu>
2019-12-18 13:28:55 -0700

    CSS/Style changes to allow adaptive text sizing for different sized viewports.

--- app.R
+++ app.R
@@ -91,8 +91,7 @@ map = leaflet() %>% addTiles() %>%
 ################################################################################
 # User interface 
 
-ui <- navbarPage("",
-                 
+ui <- navbarPage("", 
         # Landing page.  "tags$" is replicating HTML directly         
         tabPanel("Home",
                  
@@ -124,17 +123,17 @@ ui <- navbarPage("",
     sidebarLayout(
         
         # create side panel for parameter adjustment
-        sidebarPanel(
-            strong('Spatial Data Explorer', style = "font-size:36px"), br(), br(), 
+        sidebarPanel(width = 5, style = "height:90vh; font-size:1.2vh",
+            strong('Spatial Data Explorer', style = "font-size:2.5vh"), br(), br(), 
             p('The spatial explorer shows the Snake River AVA - where the majority of Idaho’s vineyards are located. The full [dataset] expands far beyond this area which can be visualized
-              by selecting the region of your choice. The plot can be zoomed in or out in the same fashion as Google Maps.', style = "font-size:18px"),
+              by selecting the region of your choice. The plot can be zoomed in or out in the same fashion as Google Maps.', style = "font-size:1.5vh"),
             p('To create a unique plot, select a climate variable (for example, temperature or precipitation) and years on the left and then click the ‘create map’ button. The historical option
-              will require a date range to be averaged or summed over (depending on the variable).', style = "font-size:18px"),
+              will require a date range to be averaged or summed over (depending on the variable).', style = "font-size:1.5vh"),
             p('Anomalies are the difference between your year of choice  and the average of that variable for all 30 years.  For example, if you select ‘Monthly Anomaly’, VARIABLE=’Minimum
               Daily Temperature’,‘YEAR=2011’ and ‘MONTH=April’ you will get a plot showing the difference between the average minimum daily temperatures for the month of April in 2011 and the
               average minimum temperatures in April for all years spanning 1988-2017. In this example, temperatures are about 2.0 - 3.5 degrees Celsius cooler in April 2011 than the 30 year
               average. If you were to look at your crop yields over the 30 year timespan, it would be interesting to see how the deviation from average in 2011 affected your yields that year!',
-              style = "font-size:18px"),
+              style = "font-size:1.5vh"),
             br(), div(class = "intro-divider3"), br(),
         
             # Input types with defaults selected 
@@ -157,9 +156,9 @@ ui <- navbarPage("",
             # call custom function for creating "Create map"button
             withBusyIndicatorUI(actionButton("button", "Create Map", class = "btn-primary"))),
         # start main panel (defaulted to right side)
-        mainPanel(
+        mainPanel(width = 7,
             # Render leaflet map
-            leafletOutput("myMap", width = "100%", height = "88vh")
+            leafletOutput("myMap", height = "90vh")
         ))),
     
         # Start Time Seeries tab
@@ -168,14 +167,14 @@ ui <- navbarPage("",
                  sidebarLayout(
                      # Inputs 
                      
-                     sidebarPanel(
-                         strong('Timeseries Data Explorer', style = "font-size:36px"), br(), br(), 
+                     sidebarPanel(width = 4, style = "height:80vh; font-size:1.2vh",
+                         strong('Timeseries Data Explorer', style = "font-size:2.5vh"), br(), br(), 
                          p('To use the time series graph, select the region, variable and up to five different years to be compared simultaneously - the graph will update automatically as inputs 
                            are changed. Mousing over the graph will display the values at that location for each value in the upper right corner along with the date.  You can zoom into these graphs
-                           by clicking and dragging across an area  on the map or by moving  the slider below the graph.', style = "font-size:18px"),
+                           by clicking and dragging across an area  on the map or by moving  the slider below the graph.', style = "font-size:1.5vh"),
                          p("Each data line represents the average value over the entire region that has been selected. These time series graphs are particularly useful for assessing climate
                            differences between different years. For example, if you select Growing Degree Days and the years 2006 and 2007 you can compare the mean cumulative GDD of each year
-                           through time. ", style = "font-size:18px"),
+                           through time. ", style = "font-size:1.5vh"),
                          br(), div(class = "intro-divider3"), br(),
             
                          selectInput("domainInput1", "Domain",
@@ -191,20 +190,20 @@ ui <- navbarPage("",
                      
                      mainPanel(
                          # "Dygraph" (interactive timeseries graph)
-                         dygraphOutput("myGraph", width = "100%", height = "60vh")
+                         dygraphOutput("myGraph", width = "100%", height = "70vh")
                      )
                  )),
         tabPanel("Statistics",
                  #titlePanel(""),
-                 sidebarPanel(
-                     strong('Statistics', style = "font-size:36px"), br(), br(),
+                 sidebarPanel(width = 4, style = "height:80vh; font-size:1.2vh",
+                     strong('Statistics', style = "font-size:2.5vh"), br(), br(),
                      p('The data table will update as  different regions or time periods are selected.  Additionally, you can click on any header within
                        the table itself to organize the table 
-                       by ascending or descending values of that variable. To export the data …', style = "font-size:18px"),
+                       by ascending or descending values of that variable. To export the data …', style = "font-size:1.5vh"),
                      p('Assessing these statistics allows you to see the total variability of each metric over 30 years and easily compare yearly
-                       summaries', style = "font-size:18px"),
+                       summaries', style = "font-size:1.5vh"),
                      p('In future versions of the climate data explorer we will add the capability for you to get these same statistics for your own
-                       area of interest!', style = "font-size:18px"),
+                       area of interest!', style = "font-size:1.5vh"),
                      br(), div(class = "intro-divider3"), br(),
                      
                      selectInput("domainInput2", "Domain",
@@ -214,41 +213,41 @@ ui <- navbarPage("",
                         choices = c("YEAR","MONTH","DAY"),
                         selected = "YEAR")),
                 mainPanel(
-                     DT::dataTableOutput("myTable", height = "75vh")
+                     DT::dataTableOutput("myTable", height = "90vh")
                  )),
         tabPanel("Background",
                  # Logos at bottom of page with links
                  tags$a(href = "https://www.boisestate.edu",target = "_blank", img(src="BSU2.png", class = "logo1")), 
                  tags$a(href = "https://agri.idaho.gov/main/", target = "_blank", img(src="ISDOA.png", class = "logo2")),
                  tags$a(href = "https://leaf.boisestate.edu/people/", target = "_blank", img(src="LEAF2.png", class = "logo3")),
-                 sidebarPanel(width = 5,
+                 sidebarPanel(width = 5, style = "height:80vh; font-size:1.2vh",
                     strong('Background', style = "font-size:36px"), br(), br(),
                     p("This project was done with through the support of a specialty crop block grant provided by the ", 
                       tags$a(href="https://agri.idaho.gov/main/", target = "_blank", "Idaho State Department of Agriculture."), 
                       "Viticulture and other specialty crops, such as hops, are rapidly emerging as significant crops in Idaho. The
                       emergence of big data and computational power allows us to determine what the historical climate was in this region at a higher
                       resolution (finer time and spatial scale) than most other products. We hope that making this data publicly available will become
-                      a valuable resource for individuals that currently, or are planning to grow crops in Idaho.", style = "font-size:18px"),
+                      a valuable resource for individuals that currently, or are planning to grow crops in Idaho.", style = "font-size:1.5vh"),
                     p("The climate data used for this project is a modeled dataset using the ",
                       tags$a(href="https://en.wikipedia.org/wiki/Weather_Research_and_Forecasting_Model", target="_blank", 
                       "Weather and Research Forecasting (WRF) model. "),  "It was generated at the ", 
                       tags$a(href="https://www.boisestate.edu/leaf/research/", target = "_blank", "LEAF lab "), 
                       "at Boise State University.  It contains two regions of interest: a larger region covering much of
                       the Pacific Northwest at 3km spatial resolution, and an inner region  at 1km spatial resolution.  Both of these have hourly
-                      data from water year (Oct 1 - September 30) 1988 through 2017.", style = "font-size:18px"),
+                      data from water year (Oct 1 - September 30) 1988 through 2017.", style = "font-size:1.5vh"),
                     p("The total size of full dataset in incredibly large - over 250 terabytes! The data we show through this app is but a very small
                       piece of the pie.  Of particular relevance is that all data shown here was aggregated from hourly to daily temporal resolution.
                       Additionally, data in the time series or statistic sections were all spatially averaged over the region of interest. The animation 
                       on the right showcases some of this data during the deadly frost (for many vineyards) in January 2017. You can clearly see the cold
                       air drain into Treasure Valley at night and stay there for a couple of days as one of our famous inversions.  You can see some grey
                       and white regions pop up at about the 0:20 mark where temperatures were -15 to -28 degrees F below zero!  ",
-                      style = "font-size:18px"),
+                      style = "font-size:1.5vh"),
                     p("If you have any questions regarding our ongoing Idaho projects or general questions please feel free to contact us! We look forward
-                      to hearing from you.", style = "font-size:18px"),
-                    strong("Charlie Becker - charles11becker@gmail.com", br(), "LEAF lab - lejoflores@boisestate.edu", style = "font-size:18px")
+                      to hearing from you.", style = "font-size:1.5vh"),
+                    strong("Charlie Becker - charles11becker@gmail.com", br(), "LEAF lab - lejoflores@boisestate.edu", style = "font-size:1.5vh")
                               ),
-                 mainPanel(width = 7,
-                    tags$video(src="Freeze_6fps.mp4", controls="controls", type = "video/mp4", width= "90%")
+                 mainPanel(width = 7, style = "height:95vh",
+                    tags$video(src="Freeze_6fps.mp4", controls="controls", type = "video/mp4", width= "95%")
                     #div(img(src="Jvictor2.jpg", width="100%"), style="text-align:center;"),
                     #br(), p("A view from Sunnyslope - January 2019", style = "font-size:18px; text-align:center"), br()
                     
